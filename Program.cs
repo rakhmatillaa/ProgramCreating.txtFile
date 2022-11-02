69b8dedeccc605d995fe74e7aa971842defed886
rakhmatillaa <abdukadyrovrahmatilla461@gmail.com>
2022-11-02 18:21:35 +0500

    created .gitignore, wrote code in Program.cs

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿string path = @"C:\Users\User\Desktop\English";
+Console.Write("Press R for read-only or W for writeable: ");
+ConsoleKeyInfo key = Console.ReadKey();
+Console.WriteLine();
+Stream? s;
+if (key.Key == ConsoleKey.R)
+{
+    s = File.Open(
+    Path.Combine(path, "file.txt"),
+    FileMode.OpenOrCreate,
+    FileAccess.Read);
+}
+else
+{
+    s = File.Open(
+    Path.Combine(path, "file.txt"),
+    FileMode.OpenOrCreate,
+    FileAccess.Write);
+}
+string message;
+switch (s)
+{
+    case FileStream writeableFile when s.CanWrite:
+        message = "The stream is a file that I can write to.";
+        break;
+    case FileStream readOnlyFile:
+        message = "The stream is a read-only file.";
+        break;
+ case MemoryStream ms:
+        message = "The stream is a memory address.";
+        break;
+    default: // always evaluated last despite its current position
+        message = "The stream is some other type.";
+        break;
+    case null:
+        message = "The stream is null.";
+        break;
+}
+Console.WriteLine(message);
